aa4410ddfe1e7c31fc9fdd18b6c371e1e2981289
SteveArevalo <s.arevalo62@gmail.com>
2023-10-04 13:08:45 -0400

    My portfolio website.

---
+++ venv/server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+
+app = Flask(__name__)
+app.debug = True
+print(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('../database.txt', mode='a') as database:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        file = database.write(f'\n{name},{email},{message}')
+
+def write_to_csv(data):
+    with open('../database.csv', mode='a', newline='\n') as database2:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return 'Form submitted! I will get in contact with you shortly. '
+    else:
+        return 'Something went wrong. Try again!'
